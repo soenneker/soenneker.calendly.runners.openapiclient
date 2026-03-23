ac668ac1f9fb01c73176be919da6d6d28e9e2d9e
Jake Soenneker <jake@soenneker.com>
2026-03-22 20:02:57 -0500

    Initial commit

---
+++ src/Soenneker.Calendly.Runners.OpenApiClient/Startup.cs
@@ -0,0 +1,31 @@
+using Microsoft.Extensions.DependencyInjection;
+using Soenneker.Managers.Runners.Registrars;
+using Soenneker.Calendly.Runners.OpenApiClient.Utils;
+using Soenneker.Calendly.Runners.OpenApiClient.Utils.Abstract;
+using Soenneker.Utils.File.Download.Registrars;
+using Soenneker.Utils.Yaml.Registrars;
+
+namespace Soenneker.Calendly.Runners.OpenApiClient;
+
+/// <summary>
+/// Console type startup
+/// </summary>
+public static class Startup
+{
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public static void ConfigureServices(IServiceCollection services)
+    {
+        services.SetupIoC();
+    }
+
+    public static IServiceCollection SetupIoC(this IServiceCollection services)
+    {
+        services.AddHostedService<ConsoleHostedService>()
+                .AddScoped<IFileOperationsUtil, FileOperationsUtil>()
+                .AddRunnersManagerAsScoped()
+                .AddFileDownloadUtilAsScoped()
+                .AddYamlUtilAsScoped();
+
+        return services;
+    }
+}
